8bde53da25a8e07d9ec3e801ff03db273da5c783
vav8085 <vav8085@gmail.com>
2016-12-07 17:26:08 -0600

    Adding initial code

---
+++ src/com/vav/Common/SinglyLinkedList.java
@@ -0,0 +1,118 @@
+package com.vav.Common;
+
+/**
+ * Created by Vaibhav on 11/9/2016.
+ */
+public class SinglyLinkedList {
+    private Link head;
+
+    public SinglyLinkedList() {
+        head = null;
+    }
+
+    public void printList() {
+        Link current = head;
+        while (current != null) {
+            System.out.print(current.getData() + ", ");
+            current = current.getNext();
+        }
+    }
+
+    public void insertStart(int data) {
+        Link link = new Link(data);
+        link.setNext(head);
+        head = link;
+    }
+
+    public void insertEnd(int data) {
+        Link link = new Link(data);
+        Link current = head;
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(link);
+
+    }
+
+    public void insertAtLocation(int data, int location) {
+        Link link = new Link(data);
+        Link current = head;
+        Link previous = current;
+        int currentLocation = 0;
+        if (location == 0) {
+            link.setNext(head);
+            head = link;
+        } else {
+            while (current != null && currentLocation < location) {
+                previous = current;
+                current = current.getNext();
+                currentLocation++;
+            }
+            previous.setNext(link);
+            link.setNext(current);
+        }
+    }
+
+    public void deleteAtStart() {
+        head = head.getNext();
+    }
+
+    public void deleteAtEnd() {
+        Link current = head;
+        Link previous = null;
+        while (current.getNext() != null) {
+            previous = current;
+            current = current.getNext();
+        }
+        previous.setNext(null);
+
+    }
+
+    public void deleteAtLocation(int location) {
+        Link current = head;
+        Link previous = current;
+        int currentLocation = 0;
+        if (location == 0) {
+            head = head.getNext();
+        } else {
+            while (current.getNext() != null && currentLocation < location) {
+                currentLocation++;
+                previous = current;
+                current = current.getNext();
+            }
+            previous.setNext(current.getNext());
+        }
+    }
+    public void getHead(){
+        System.out.print(head.getData());
+    }
+}
+
+
+
+class Link {
+    private int data;
+    private Link next;
+
+    public Link(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Link getNext() {
+        return next;
+    }
+
+    public void setNext(Link next) {
+        this.next = next;
+    }
+
+
+}
---
+++ src/com/vav/designPatterns/IteratorPattern.java
@@ -0,0 +1,7 @@
+package com.vav.designPatterns;
+
+/**
+ * Created by Vaibhav on 12/7/2016.
+ */
+public class IteratorPattern {
+}
