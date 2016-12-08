dbf4db787e0c6a5b2563d774053aae333fa62ec1
vav8085 <vav8085@gmail.com>
2016-12-08 11:54:22 -0600

    Added log files

---
+++ src/com/vav/karumanchi/LinkedList/NthNodeEndLl.java
@@ -0,0 +1,20 @@
+package com.vav.karumanchi.LinkedList;
+
+import com.vav.Common.SinglyLinkedList;
+
+/**
+ * Created by Vaibhav on 12/7/2016.
+ */
+public class NthNodeEndLl {
+     private  static SinglyLinkedList singlyLinkedList= new SinglyLinkedList();
+    public static void main(String arg[]){
+        singlyLinkedList.insertStart(5);
+        singlyLinkedList.insertStart(4);
+        singlyLinkedList.insertStart(3);
+        singlyLinkedList.insertStart(2);
+        singlyLinkedList.insertStart(1);
+        singlyLinkedList.nThNodeFromEndTwoIter(2);
+    }
+
+
+}
