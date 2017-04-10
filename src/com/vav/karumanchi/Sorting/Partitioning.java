9191544663d8b88196b6d90a7e03fc5b9e298613
vaibhav pathak <vav8085@gmail.com>
2017-04-10 16:26:22 -0500

    Partition complete

--- src/com/vav/karumanchi/Sorting/Partitioning.java
+++ src/com/vav/karumanchi/Sorting/Partitioning.java
@@ -4,23 +4,39 @@ package com.vav.karumanchi.Sorting;
  * Created by Vaibhav on 4/9/2017.
  */
 public class Partitioning {
-    static int arr[] = {10,11,2,1,9,8,7,6,5,4,3};
+    static int arr[] = {4,3,2,1};
     public static void main(String arg[]){
        // mergeSort.mergeSort(arr);
+
+        int partition = partitionIt(0,arr.length-1,arr.length/2);
+        System.out.println("Partition:"+partition);
         for (int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+ ", ");
         }
-        int partition = partitionit(arr[0],arr[arr.length-1],arr.length/2);
+
     }
 
-    private static int partitionit(int start, int end, int partition) {
+    private static int partitionIt(int start, int end, int partition) {
         int left = start-1;
         int right = end+1;
-        while(left<right && arr[left++]<arr[partition]);
-        while (right>left && arr[right--]>arr[partition]);
+        while (true) {
+            while (left < right && arr[++left] < arr[partition]) ;
+            while (right > left && arr[--right] > arr[partition]) ;
+            if(left>=right){
+                break;
+            }else{
+                swap(left,right);
+            }
+        }
 
 
         return arr[partition];
+    }
+
+    private static void swap(int one, int two) {
+        int temp=arr[one];
+        arr[one]=arr[two];
+        arr[two]=temp;
     }
 
 }
