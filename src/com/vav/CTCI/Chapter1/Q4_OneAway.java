9280bd86bde3cb21194b9649800fdb426b850bcd
vaibhav pathak <vav8085@gmail.com>
2017-04-25 20:48:11 -0500

    Q4 CTCI

---
+++ src/com/vav/CTCI/Chapter1/Q4_OneAway.java
@@ -0,0 +1,16 @@
+package com.vav.CTCI.Chapter1;
+
+/**
+ * Created by Vaibhav on 4/25/17.
+ */
+public class Q4_OneAway {
+    public static void main(String arg[]){
+
+    }
+    //Using Hashmaps
+    public static boolean oneAway(String s1, String s2){
+
+
+        return false;
+    }
+}
---
+++ src/com/vav/CTCI/Chapter1/QX_StringAnagrams.java
@@ -0,0 +1,31 @@
+package com.vav.CTCI.Chapter1;
+
+import java.util.Arrays;
+
+/**
+ * Created by Vaibhav on 4/25/17.
+ * This question is same as String permutations Q2
+ */
+public class QX_StringAnagrams {
+    public static void main(String arg[]){
+        System.out.println(checkAnagrams("carrr","car"));
+    }
+    //USING SORTING
+    public static boolean checkAnagrams(String s1, String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        }else {
+            char[] s1Chars = s1.toCharArray();
+            char[] s2Chars = s2.toCharArray();
+            Arrays.sort(s1Chars);
+            Arrays.sort(s2Chars);
+            for(int i=0;i<s1Chars.length;i++){
+                if(s1Chars[i]!=s2Chars[i]){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
