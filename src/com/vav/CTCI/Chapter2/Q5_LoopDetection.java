9f707a77b4523f34c76d50404677135bcd04f4d1
vaibhav pathak <vav8085@gmail.com>
2017-05-11 12:19:56 -0500

    Q4 and Q5 chapter 2

---
+++ src/com/vav/CTCI/Chapter2/Q5_LoopDetection.java
@@ -0,0 +1,21 @@
+package com.vav.CTCI.Chapter2;
+
+import com.vav.CTCI.Common.LinkedList.Link;
+import com.vav.CTCI.Common.LinkedList.LinkedList;
+
+/**
+ * Created by Vaibhav on 5/11/17.
+ */
+public class Q5_LoopDetection {
+    public Link loopDetection(LinkedList linkedList){
+        Link first = linkedList.getHead(), second = linkedList.getHead();
+        while(second.getNext().getNext()!=null){
+            if(first.getData() == second.getData()){
+                return first;
+            }
+            first = first.getNext();
+            second = second.getNext().getNext();
+        }
+             return null;
+    }
+}
