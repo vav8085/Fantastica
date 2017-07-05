ab9bf60c7d32e714c16eb465816b149d08e0c33b
vaibhav pathak <vav8085@gmail.com>
2017-07-05 17:30:00 -0500

    Changed graph

--- src/com/vav/Common/Graphs/Graph.java
+++ src/com/vav/Common/Graphs/Graph.java
@@ -8,18 +8,16 @@ import java.util.Stack;
 public class Graph {
     private final int MAX_VERTICES = 20;
     private Vertex[] vertexList;
-    private int adjMatrix[][];
+    private int[][] adjacencyMatrix;
     private int nVerts;
-    private Stack<Integer> stack;
 
     public Graph(){
-        stack = new Stack<>();
         vertexList = new Vertex[MAX_VERTICES];
-        adjMatrix = new int[MAX_VERTICES][MAX_VERTICES];
+        adjacencyMatrix = new int[MAX_VERTICES][MAX_VERTICES];
         nVerts = 0;
         for(int i = 0; i < MAX_VERTICES; i++){
             for(int j = 0; j< MAX_VERTICES; j++){
-                adjMatrix[i][j] = 0;
+                adjacencyMatrix[i][j] = 0;
             }
         }
     }
@@ -27,34 +25,16 @@ public class Graph {
         vertexList[nVerts++] = new Vertex(label);
     }
     public void addEdge(int start, int end){
-        adjMatrix[start][end] = 1;
-        adjMatrix[end][start] = 1;
+        adjacencyMatrix[start][end] = 1;
+        adjacencyMatrix[end][start] = 1;
     }
     public void displayVertex(int vertexPosition){
         System.out.println(vertexList[vertexPosition]);
     }
 
-    public int getAdjUnvisitedVertex(int v){
-        for(int j=0;j<nVerts;j++){
-            if(adjMatrix[v][j]==1 && vertexList[j].isWasVisited()==false){
-                return j;
-            }
+    public void getAdjascentUnvisitedVertex(int v){
+        for(int j=0;j<nVerts;j++) {
+            if (adjacencyMatrix[v][j]==1 && )
         }
-        return -1;
-    }
-    public void dfs(){
-        vertexList[0].setWasVisited(true);
-        displayVertex(0);
-        stack.push(0);
-
-        while (!stack.isEmpty()){
-            int v = getAdjUnvisitedVertex(stack.peek());
-            if(v==-1){
-
-            }else{
-
-            }
-        }
-
     }
 }
