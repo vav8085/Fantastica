b78c377a46a066a93d131baf5b5a5609b7981d65
vaibhav pathak <vav8085@gmail.com>
2017-08-07 16:18:37 -0500

    Weighted quick union

--- src/com/vav/Common/Algorithms1_Sedgewick/WeightedQuickUnion.java
+++ src/com/vav/Common/Algorithms1_Sedgewick/WeightedQuickUnion.java
@@ -5,7 +5,33 @@ package com.vav.Common.Algorithms1_Sedgewick;
  */
 public class WeightedQuickUnion {
     private int id[];
+    private int sz[];
     public WeightedQuickUnion(int n){
-
+        for(int i=0;i<n;i++){
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+    public int getRoot(int item){
+        while(id[item]!=item){
+            item = id[item];
+        }
+        return item;
+    }
+    public boolean connected(int p , int q){
+        if(getRoot(p)==getRoot(q)){
+            return true;
+        }else return false;
+    }
+    public void union(int p, int q){
+        int pr = getRoot(p);
+        int qr = getRoot(q);
+        if(sz[p]<sz[q]){
+            id[q] = pr;
+            sz[p] = sz[p] + sz[q];
+        }else{
+            id[p] = qr;
+            sz[q] = sz[p] + sz[q];
+        }
     }
 }
