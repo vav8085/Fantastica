de41a499f7a52cd4fdc02ab4b0e544e44e71dc32
Vaibhav <vav8085@gmail.com>
2017-09-28 20:45:39 -0500

    Preorder traversal

--- src/com/vav/Common/Trees/BinaryTrees/IntegerBinaryTree.java
+++ src/com/vav/Common/Trees/BinaryTrees/IntegerBinaryTree.java
@@ -1,5 +1,7 @@
 package com.vav.Common.Trees.BinaryTrees;
 
+import java.util.Stack;
+
 /**
  * Created by Vaibhav on 9/24/2017.
  */
@@ -17,9 +19,30 @@ public class IntegerBinaryTree {
             preorder(root.getLeftNode());
             preorder(root.getRightNode());
         }
-
     }
+    /*
+        1.  We first create a stack
+        2.  we then push the root element onto the stack
+        3.  Now we loop until the stack is empty and pop top element and print it
+        4.  We also push the right element first and then the left element. The right element remain at bottom
+        5.  For every node we first print it then push its left node and then push its right node
+        6.  This way every time the root is prited then its left subtree and then its right subtres
+     */
+    public void preorderIterative(IntegerBinaryTreeNode root){
 
+        Stack<IntegerBinaryTreeNode> stack = new Stack<>();
+        stack.push(root);
+      while(!stack.isEmpty()){
+          IntegerBinaryTreeNode temp = stack.pop();
+          System.out.println(temp.getData());
+          if(temp.getRightNode()!=null){
+              stack.push(temp.getRightNode());
+          }
+          if(temp.getLeftNode()!=null){
+              stack.push(temp.getLeftNode());
+          }
+      }
+    }
     public void find(int id){
 
     }
