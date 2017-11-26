8ce087117018b4bcd2e2d749b3e791c154a9aa3f
Vaibhav <vav8085@gmail.com>
2017-11-26 09:56:41 -0600

    Nth node recursive —INCOMPLETE—

---
+++ src/com/vav/karumanchi/LinkedList_03/Q6_NthNodeFromEndRecursive.java
@@ -0,0 +1,28 @@
+package com.vav.karumanchi.LinkedList_03;
+
+import com.vav.CTCI.Common.LinkedList.Link;
+import com.vav.CTCI.Common.LinkedList.LinkedList;
+
+/**
+ * Created by vaibhav on 11/26/17.
+ */
+public class Q6_NthNodeFromEndRecursive {
+    public static void main(String arg[]){
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.insertAtEnd(1);
+        ll.insertAtEnd(2);
+        ll.insertAtEnd(3);
+        ll.insertAtEnd(4);
+        ll.insertAtEnd(5);
+        ll.insertAtEnd(6);
+        ll.insertAtEnd(7);
+        //System.out.println(nthNodeFromEndOneRecur(ll.getHead(),1,0));
+
+    }
+
+//    private static int nthNodeFromEndOneRecur(Link<Integer> head, int position, int count) {
+//        if(head!=null){
+//
+//        }
+//    }
+}
