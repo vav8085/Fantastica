d5c0bf35038e8c3de4cda2b60804ad20ae52ffd9
vaibhav pathak <vav8085@gmail.com>
2017-11-30 13:18:06 -0600

    Insert node in sorted LL

---
+++ src/com/vav/karumanchi/LinkedList_03/Q16_InsertNodeInSortedLl.java
@@ -0,0 +1,54 @@
+package com.vav.karumanchi.LinkedList_03;
+
+import com.vav.CTCI.Common.LinkedList.Link;
+
+/**
+ * Created by Vaibhav on 11/30/17.
+ */
+public class Q16_InsertNodeInSortedLl {
+
+    public static void main(String arg[]){
+        Link<Integer> link1 = new Link<>(1);
+        Link<Integer> link2 = new Link<>(2);
+        //Link<Integer> link3 = new Link<>(3);
+        Link<Integer> link4 = new Link<>(4);
+        Link<Integer> link5 = new Link<>(5);
+        Link<Integer> link6 = new Link<>(6);
+
+        link1.setNext(link2);
+        link2.setNext(link4);
+        link4.setNext(link5);
+        link5.setNext(link6);
+        link6.setNext(null);
+        Link<Integer> head = insertNodeInSortedLl(link1,3);
+        while (head!=null){
+            System.out.print(head.getData());
+            head = head.getNext();
+        }
+
+    }
+    public static Link<Integer> insertNodeInSortedLl(Link<Integer> head, int nodeData){
+        Link<Integer> nodeToInsert = new Link<>(nodeData);
+
+        Link<Integer> current = head;
+        Link<Integer> previous = current;
+        if(head==null){
+            return nodeToInsert;
+        }
+        while (current!=null){
+            previous = current;
+            current = current.getNext();
+            if(previous.equals(head) && nodeData < previous.getData()){
+                nodeToInsert.setNext(previous);
+                head = nodeToInsert;
+                return head;
+            }
+            if(previous.getData()<nodeData && current!=null?current.getData() > nodeData:true){
+                previous.setNext(nodeToInsert);
+                nodeToInsert.setNext(current);
+                return head;
+            }
+        }
+        return head;
+    }
+}
