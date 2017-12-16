893f1cbce6574c7c7f2dbb69e4cd31d5610d90cc
Vaibhav <vav8085@gmail.com>
2017-12-16 10:19:44 -0600

    counting sort

--- src/com/vav/Common/Sorting/CountingSort.java
+++ src/com/vav/Common/Sorting/CountingSort.java
@@ -9,7 +9,7 @@ public class CountingSort {
 
     }
     public static int[] countingSort(int[] inputArr){
-
+        // { 1, 3, 1, 7, 5, 5, 9, 7 }
         int[] outputArr = new int[inputArr.length];
 
         int[] countingArr = new int[10]; //The size of countingArr should be the maximum value in inputArr but lets take it 10 for now
@@ -24,6 +24,10 @@ public class CountingSort {
             countingArr[k] = countingArr[k] + countingArr[k-1]; //adding values to the previous and saving it
         }
         for(int l=0;l<outputArr.length;l++){
+            // in case of l=0, inputArr[0] = 1, countArr[1] = 2, outputArr[1] = 1
+            // in case of l=1, inputArr[1] = 3, countArr[3] = 3-1 = 2, outputArr[2] = 3
+            // in case of l=3, inputArr[3] = 1, countArr[1] = 1-1 = 0, outputArr[0] = 1
+            // in case of l=4, inputArr[4] = 7, countArr[7] = 6-1 = 5, outputArr[5] = 7
             outputArr[countingArr[inputArr[l]]-1] = inputArr[l];
             --countingArr[inputArr[l]];
         }
