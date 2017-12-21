0625a9bff5a41eed4b6f8da4ba46cce179134be7
Vaibhav <vav8085@gmail.com>
2017-12-21 07:20:16 -0600

    palindrome ll incomplete

---
+++ src/com/vav/karumanchi/Stacks_04/Q9_CheckPalindromeLinkedList.java
@@ -0,0 +1,14 @@
+package com.vav.karumanchi.Stacks_04;
+
+/**
+ * Created by vaibhav on 12/21/17.
+ */
+public class Q9_CheckPalindromeLinkedList {
+    //we can do it with the help of a hashmap by traversing the linked list and saving all items in a hashmap with their counts
+    //if there is more than one count that is odd then its not a palindrome
+
+
+    //We can do this with the help of a stack by first saving all items to the stack and then again scanning the list
+    //and match the popped item with the list items. If they are same then its not a stack.
+
+}
