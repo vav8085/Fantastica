023d1ecb7aa3d503ecc66000a6676ea495eb5f57
Vaibhav <vav8085@gmail.com>
2017-12-30 09:46:10 -0600

    Separate chaining hash map

--- src/com/vav/Common/Hashing/LinearProbingHashMap.java
+++ src/com/vav/Common/Hashing/LinearProbingHashMap.java
@@ -29,12 +29,34 @@ public class LinearProbingHashMap {
         int hashedValue = hashFunction(key); //hashvalue = location
         while (hashArray[hashedValue]!=null && hashArray[hashedValue].getKey()!=-1){ //if current cell is occupied or nothing deleted here
             ++hashedValue; //then go to next value
+            if(hashedValue==hashArray.length) // I have put this condition because i dont know if wrapping arround is necessary every loop iteration
             hashedValue = hashedValue % size; // this is for wrapping around if you reach last array value, this will return 0
         }
         hashArray[hashedValue] = data; // insert the data at this location
     }
     public void delete(int key){
-
+        int hashValue = hashFunction(key);
+        while (hashArray[hashValue]!=null){
+            if(hashArray[hashValue].getKey()==key){
+                hashArray[hashValue]=deletedPlaceholder;
+                return;
+            }
+                ++hashValue;
+                if(hashValue==size)
+                    hashValue = hashValue % size;
+        }
+    }
+    public boolean find(int key){
+        int hashVal = hashFunction(key);
+        while (hashArray[hashVal]!=null){
+            if(hashArray[hashVal].getKey()==key){
+                return true;
+            }
+            ++hashVal;
+            if(hashVal==size)
+                hashVal = hashVal % size;
+        }
+        return false;
     }
 }
 
