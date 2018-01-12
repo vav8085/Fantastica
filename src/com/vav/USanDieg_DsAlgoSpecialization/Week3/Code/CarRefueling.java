041441888ec8d644992d240fdb343b32555b3590
Vaibhav <vav8085@gmail.com>
2018-01-11 21:07:37 -0600

    error condition

--- src/com/vav/USanDieg_DsAlgoSpecialization/Week3/Code/CarRefueling.java
+++ src/com/vav/USanDieg_DsAlgoSpecialization/Week3/Code/CarRefueling.java
@@ -6,24 +6,25 @@ package com.vav.USanDieg_DsAlgoSpecialization.Week3.Code;
 public class CarRefueling {
 
     public static void main(String arg[]){
-        GasStation[] arr = new GasStation[6];
+        GasStation[] arr = new GasStation[7];
         int maxTravel = 400;
-        arr[0] = new GasStation(120,"G1");
-        arr[1] = new GasStation(350,"G2");
-        arr[2] = new GasStation(500,"G3");
-        arr[3] = new GasStation(700,"G4");
-        arr[4] = new GasStation(900,"G5");
-        arr[5] = new GasStation(1000,"G6");
+        arr[0]=new GasStation(0,"G0");
+        arr[1] = new GasStation(120,"G1");
+        arr[2] = new GasStation(350,"G2");
+        arr[3] = new GasStation(500,"G3");
+        arr[4] = new GasStation(600,"G4");
+        arr[5] = new GasStation(750,"G5");
+        arr[6] = new GasStation(1000,"G6");
 
         System.out.print(optimalRefuels(maxTravel,arr));
 
     }
-    //this is not printing correct for 120,350,500,750,900,1000, need to check
+
     private static String optimalRefuels(int maxTravel, GasStation[] arr) {
         String s="";
         //ToDo write logic
         GasStation current=arr[0];
-        GasStation previous = new GasStation(0,"G0");
+        GasStation previous = arr[0];
         s = s+previous.getValue();
         for(int i=0;i<=arr.length;i++){
             while(i<arr.length && (previous.getDistance()+maxTravel)>=arr[i].getDistance()){
