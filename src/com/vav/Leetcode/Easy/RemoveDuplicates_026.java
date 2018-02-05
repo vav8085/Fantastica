d9ebab5eafcd599a436087f59160fbba342ccc11
Vaibhav <vav8085@gmail.com>
2018-02-05 00:32:07 -0600

    remove dups from sorted array

---
+++ src/com/vav/Leetcode/Easy/RemoveDuplicates_026.java
@@ -0,0 +1,21 @@
+package com.vav.Leetcode.Easy;
+
+/**
+ * Created by vaibhav on 2/5/18.
+ */
+public class RemoveDuplicates_026 {
+
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int j=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[j]){
+                j++;
+                nums[j]=nums[i];
+            }
+        }
+        return j+1;
+    }
+}
