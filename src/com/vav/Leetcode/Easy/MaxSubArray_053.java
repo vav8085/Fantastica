f50ae521aed5a6e0fb7c20e51686e2220dfd8775
Vaibhav <vav8085@gmail.com>
2018-02-06 22:33:09 -0600

    maximum subarray

---
+++ src/com/vav/Leetcode/Easy/MaxSubArray_053.java
@@ -0,0 +1,26 @@
+package com.vav.Leetcode.Easy;
+
+/**
+ * Created by vaibhav on 2/6/18.
+ */
+public class MaxSubArray_053 {
+
+    public static void main(String arg[]){
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maximumSubArray(arr));
+    }
+    public static int maximumSubArray(int[] arr){
+        int sumTillNow = 0;
+        int totalSum = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(sumTillNow<0){
+                sumTillNow=0;
+            }
+            sumTillNow = sumTillNow + arr[i];
+            if(sumTillNow>totalSum){
+                totalSum = sumTillNow;
+            }
+        }
+        return totalSum;
+    }
+}
