2dccfd6237cf49d9ffa0d73bc0d9e29fd23f98d0
vaibhav pathak <vav8085@gmail.com>
2018-03-05 10:47:22 -0600

    dynamic programming basics

--- src/com/vav/USanDieg_DsAlgoSpecialization/Week3/Code/MinimumChange.java
+++ src/com/vav/USanDieg_DsAlgoSpecialization/Week3/Code/MinimumChange.java
@@ -5,14 +5,14 @@ package com.vav.USanDieg_DsAlgoSpecialization.Week3.Code;
  */
 public class MinimumChange {
     public static void main(String arg[]){
-        System.out.print(getChange(11));
+        System.out.print(getChange(40));
     }
     private static int getChange(int m) {
         //write your code here
         int arr[] = new int[3];
-        arr[0]=10;
-        arr[1]=5;
-        arr[2]=1;
+        arr[0]=25;
+        arr[1]=20;
+        arr[2]=5;
         int count=0;
         int reminder=m;
         for(int i=0;i<arr.length && reminder!=0;i++){
