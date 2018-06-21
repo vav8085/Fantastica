dd340a4dc621c9d2071df0445635ceb72c924380
Vaibhav Pathak <vav8085@gmail.com>
2018-06-21 19:21:16 -0400

    Add binary

--- src/com/vav/Leetcode/Easy/AddBinary_67.java
+++ src/com/vav/Leetcode/Easy/AddBinary_67.java
@@ -5,14 +5,27 @@ package com.vav.Leetcode.Easy;
  */
 public class AddBinary_67 {
     public static void main(String arg[]){
-
+        System.out.print(addBinary("110","11"));
     }
     public static String addBinary(String a, String b){
         /**
          * To solve this we need to know each digit of both the binary numbers to sum them
          * We can start from end of the strings and keep adding numbers and finally add carry to it
-         * if one of the string is shorter we will
+         * if one of the string is shorter we will assume that it has zeros there
          */
-        return a+b;
+        StringBuilder sumString = new StringBuilder();
+        int i= a.length()-1;
+        int j= b.length()-1;
+        int carry = 0;
+        while(i>=0 || j>=0){
+            int sum = carry;
+            sum = sum +((i>=0)? (a.charAt(i--)-'0'):0);
+            sum = sum +((j>=0)? (b.charAt(j--)-'0'):0);
+            sumString.append(sum%2);
+            carry = sum / 2;
+        }
+        if(carry==1)
+            sumString.append(carry);
+        return sumString.reverse().toString();
     }
 }
