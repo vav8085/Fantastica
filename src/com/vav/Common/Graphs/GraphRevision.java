ca21e90707a98507cadb0c6473be003e3140291f
vav8085 <vav8085@gmail.com>
2018-12-24 10:33:25 -0500

    Graph reevision

--- src/com/vav/Common/Graphs/GraphRevision.java
+++ src/com/vav/Common/Graphs/GraphRevision.java
@@ -18,6 +18,26 @@ public class GraphRevision {
         stack = new Stack();
         queue = new ArrayDeque();
         arr = new Vertex[max];
-
+        for(int i=0;i<adj.length;i++){
+            for(int j=0;j<adj.length;j++){
+                adj[i][j]=0;
+            }
+        }
     }
+    public void addVertex(char label){
+        arr[count++]=new Vertex(label);
+    }
+
+    /**
+     * In actual case we will be given two vertices and will be asked to connect them
+     * WWe have to find out the position of each vertex in the vertex array and then pass
+     * the position to below method
+     * @param i
+     * @param j
+     */
+    public void addEdge(int i, int j){
+        adj[i][j] = 1;
+        adj[j][i] = 1;
+    }
+
 }
