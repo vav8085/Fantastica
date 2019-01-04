f91253c8fb2163122e36bd3eb611135ae2bbf012
Vaibhav Pathak <vav8085@gmail.com>
2019-01-04 11:13:54 -0500

    topo sorting of directed acyclic graphs

--- src/com/vav/Common/DirectedGraph/DirGraphAdjList.java
+++ src/com/vav/Common/DirectedGraph/DirGraphAdjList.java
@@ -2,6 +2,7 @@ package com.vav.Common.DirectedGraph;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * ToDo Directed graph using adjacency list
@@ -11,6 +12,7 @@ public class DirGraphAdjList {
     private ArrayList<LinkedList<Integer>> adjacencyList;
     private final int MAX;
     private int count;
+    private Stack<Integer> topoStack;
 
     public DirGraphAdjList(int max){
         this.MAX = max;
@@ -27,11 +29,39 @@ public class DirGraphAdjList {
     public void addEdge(int v, int w){
         adjacencyList.get(v).addLast(w); //Only adding to one list
     }
-    public void getAdjacentVertices(int v){
-
+    public LinkedList<Integer> getAdjacentVertices(int v){
+       return adjacencyList.get(v);
     }
     public void dfs(int v){
         vertexArray[v].setVisited(true);
+        for(Integer adjacentVertex: getAdjacentVertices(v)){
+            if(!vertexArray[adjacentVertex.intValue()].isVisited()){
+                dfs(adjacentVertex);
+            }
+        }
+    }
+
+    /**
+     * A helper class for topological sorting this class calls the topologicalSorting method
+     * @param v
+     */
+    public void topologicalSortHelper(int v){
+        topoStack = new Stack<>();
+
+        for(int i=0;i<vertexArray.length;i++){
+            if(!vertexArray[i].isVisited())
+            topologicalSorting(i);
+        }
 
     }
+    public void topologicalSorting(int v){
+        vertexArray[v].setVisited(true);
+        for(Integer adjacentVertex : getAdjacentVertices(v)){
+            if(!vertexArray[adjacentVertex].isVisited()){
+                topologicalSorting(adjacentVertex);
+            }
+        }
+        topoStack.push(v);
+    }
+
 }
