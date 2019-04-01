38fc67e7de766fafde978314dcb0f02f9036b58b
vav8085 <vav8085@gmail.com>
2019-03-31 23:35:04 -0400

    program execution

---
+++ src/com/vav/CoursesBooks/iCamp/A_Basics/NumbersSumToValue_02.java
@@ -0,0 +1,17 @@
+package com.vav.CoursesBooks.iCamp.A_Basics;
+
+public class NumbersSumToValue_02 {
+    /**
+     * We are trying to find the numbers that sum to a value from an array
+     * The strategy is to sort the array and then go from both sides and take the sum
+     * if the sum is greater than the number than decrease the value from end and retry
+     * otherwise decrease the value from start and retry
+     * when start >= end then exit
+     * @param value
+     * @param arr
+     * @return
+     */
+    public static int[] numbersThatSumTo(int value, int[] arr){
+        return arr;
+    }
+}
