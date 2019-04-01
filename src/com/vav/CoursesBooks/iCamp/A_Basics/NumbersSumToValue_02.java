8619ca49d75d028c71b84ccd8d7f4cd3957c1fda
Vaibhav Pathak <vav8085@gmail.com>
2019-04-01 15:47:00 -0400

    move zeroes to last, even clone

--- src/com/vav/CoursesBooks/iCamp/A_Basics/NumbersSumToValue_02.java
+++ src/com/vav/CoursesBooks/iCamp/A_Basics/NumbersSumToValue_02.java
@@ -1,5 +1,7 @@
 package com.vav.CoursesBooks.iCamp.A_Basics;
 
+import java.util.Arrays;
+
 public class NumbersSumToValue_02 {
     /**
      * We are trying to find the numbers that sum to a value from an array
@@ -12,6 +14,23 @@ public class NumbersSumToValue_02 {
      * @return
      */
     public static int[] numbersThatSumTo(int value, int[] arr){
+        Arrays.sort(arr);
+        for(int i=0,j=arr.length-1; i<j;){
+            if(arr[i]+arr[j]==value){
+                return new int[]{arr[i], arr[j]};
+            }else if(arr[i]+arr[j]<value){
+                i++;
+            }else{
+                j--;
+            }
+        }
         return arr;
+    }
+    public static void main(String arg[]){
+        int arr[] = {2,8,4,9,11,21};
+        int result[] = numbersThatSumTo(13,arr);
+        for(int i=0;i<result.length;i++){
+            System.out.println(result[i]);
+        }
     }
 }
