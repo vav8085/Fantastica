30673b7b7bc4778f03ca99c8bbf9d8aa58ac512b
Vaibhav Pathak <vav8085@gmail.com>
2019-04-18 10:10:16 -0400

    Revision Apr 17 2019

--- src/com/vav/Revision/Revision_04_17_2019/Arrays/SubstituteEvenWithOneMore.java
+++ src/com/vav/Revision/Revision_04_17_2019/Arrays/SubstituteEvenWithOneMore.java
@@ -2,7 +2,7 @@ package com.vav.Revision.Revision_04_17_2019.Arrays;
 
 public class SubstituteEvenWithOneMore {
     public static int[] substituteEvenWithOneMore(int[] input){
-        
+
         return input;
     }
     public static void main(String arg[]){
