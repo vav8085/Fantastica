9f38dec826f6095a0be6380008cf01f7e5d80fc3
Vaibhav Pathak <vav8085@gmail.com>
2019-04-19 16:28:11 -0400

    karumanchi trees

---
+++ src/com/vav/CoursesBooks/karumanchi/Trees_06/BinaryTreeInsert_05_06.java
@@ -0,0 +1,19 @@
+package com.vav.CoursesBooks.karumanchi.Trees_06;
+
+import com.vav.Common.Trees.BinaryTrees.IntegerBinaryTreeNode;
+
+public class BinaryTreeInsert_05_06 {
+    public static void main(String arg[]){
+
+    }
+
+    /**
+     * Go level order, if left node is null then insert there, if right is null then insert there
+     * keep a pointer for parent
+     * @param value
+     * @param root
+     */
+    public static void binaryTreeInsert(int value, IntegerBinaryTreeNode root){
+
+    }
+}
---
+++ src/com/vav/Revision/Revision_04_18_2019/Searching/rSearchTechniques.java
@@ -0,0 +1,4 @@
+package com.vav.Revision.Revision_04_18_2019.Searching;
+
+public class rSearchTechniques {
+}
