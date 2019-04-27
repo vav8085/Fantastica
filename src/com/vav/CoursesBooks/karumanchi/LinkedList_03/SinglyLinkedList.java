bbcc968c5a6a89b890c5c46256fe94e1db56bd92
Vaibhav Pathak <vav8085@gmail.com>
2019-04-27 09:12:41 -0400

    singly linked list

--- src/com/vav/CoursesBooks/karumanchi/LinkedList_03/SinglyLinkedList.java
+++ src/com/vav/CoursesBooks/karumanchi/LinkedList_03/SinglyLinkedList.java
@@ -10,20 +10,23 @@ public class SinglyLinkedList {
     public void insert(int data){
         Node node = new Node();
         node.setData(data);
+        Node current = head;
         if(head==null){
             head = node;
         }else{
-            while (head.getNext()!=null){
-                head = head.getNext();
+            while (current.getNext()!=null){
+                current = current.getNext();
             }
-            head.setNext(node);
+            current.setNext(node);
         }
     }
     public void print(){
-        while (head!=null){
-            System.out.println(head.getData());
-            head = head.getNext();
+        Node current = head;
+        while (current!=null){
+            System.out.println(current.getData());
+            current = current.getNext();
         }
+        System.out.println();
     }
     public void insertAtStart(int data){
         Node node = new Node();
@@ -32,10 +35,60 @@ public class SinglyLinkedList {
             node.setNext(head.getNext());
         head = node;
     }
+    public void insertAtLocation(int data, int location){
+        Node node = new Node();
+        node.setData(data);
+        Node current = head;
+        Node previous = current;
+        int currentlocation = 1;
+        if(location==1){
+            node.setNext(head);
+            head = node;
+        }
+        while (current!=null){
+            if(currentlocation == location){
+                break;
+            }
+            currentlocation++;
+            previous = current;
+            current = current.getNext();
+        }
+        previous.setNext(node);
+        node.setNext(current);
+    }
     public void deleteNode(int position){
         Node current = head;
-        Node previous = head;
-        
+        Node previous = current;
+        int currentPosition = 1;
+        if(position==1){
+            head = head.getNext();
+            return;
+        }
+        while(current!=null){
+            if(currentPosition==position){
+                break;
+            }
+            currentPosition++;
+            previous = current;
+            current = current.getNext();
+        }
+        if(current!=null)
+            previous.setNext(current.getNext());
+    }
+    public static void main(String arg[]){
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.insert(1);
+        singlyLinkedList.insert(2);
+        singlyLinkedList.insert(3);
+        singlyLinkedList.insert(4);
+        singlyLinkedList.insert(5);
+        singlyLinkedList.insert(6);
+        singlyLinkedList.print();
+        singlyLinkedList.deleteNode(4);
+        singlyLinkedList.print();
+        singlyLinkedList.insertAtLocation(4,4);
+        singlyLinkedList.print();
+
     }
 }
 class Node{
