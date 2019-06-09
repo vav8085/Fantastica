40a2b93906a5a3c532317c7322986232d808b44d
vav8085 <vav8085@gmail.com>
2019-06-09 14:38:44 -0400

    Rabin karp algo implementation

--- src/com/vav/Common/Strings/RabinKarpStringSearch.java
+++ src/com/vav/Common/Strings/RabinKarpStringSearch.java
@@ -1,10 +1,22 @@
 package com.vav.Common.Strings;
 
 public class RabinKarpStringSearch {
-    public void rabinKarpStringSearch(int str, int target){
+    private static final int X = 53;
+    public int rabinKarpStringSearch(String str, String target){
         //First we will identify the initial hash of the str and the hash of our target
         //We will do this by looping over str and target for the length of our target
-
-
+        int hashTarget=0, hash=0;
+        for(int i=0;i<target.length();i++){
+            hashTarget = hashTarget*X + target.charAt(i);
+            hash = hash*X + str.charAt(i);
+        }
+        //If initial hashes match then check each character in both strings and if they also match then return 0th index
+        if(hash==hashTarget){
+            if(target.contentEquals(str.substring(0,target.length()-1))){
+                return 0;
+            }
+        }
+        //Check for other substrings in str here
+        //ToDo CHeck other strings
     }
 }
