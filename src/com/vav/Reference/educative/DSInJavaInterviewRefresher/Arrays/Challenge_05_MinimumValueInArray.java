e36b530e523620dd4747e3ed85cb44e5a9eeae3a
Vaibhav Pathak <vav8085@gmail.com>
2019-07-28 19:57:43 -0400

    minimum value in array

---
+++ src/com/vav/Reference/educative/DSInJavaInterviewRefresher/Arrays/Challenge_05_MinimumValueInArray.java
@@ -0,0 +1,16 @@
+package com.vav.Reference.educative.DSInJavaInterviewRefresher.Arrays;
+
+public class Challenge_05_MinimumValueInArray {
+    public static int minimumValueInArray(int[] arr){
+        int minimum = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<minimum){
+                minimum = arr[i];
+            }
+        }
+        return minimum;
+    }
+    public static void main(String arg[]){
+        System.out.println(minimumValueInArray(new int[]{3,6,2,8,1,9}));
+    }
+}
