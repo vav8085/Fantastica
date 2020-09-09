1715e1664db5532bc93f91f3024eec3a03b3afa1
Vaibhav Pathak <vav8085@gmail.com>
2020-09-09 01:05:42 -0400

    trapping water using dp

--- src/com/vav/Reference/Leetcode/arrays/green/TrappingRainWater_42.java
+++ src/com/vav/Reference/Leetcode/arrays/green/TrappingRainWater_42.java
@@ -22,9 +22,20 @@ import java.util.List;
  */
 public class TrappingRainWater_42 {
     public static void main(String arg[]){
-        int value = totalWater(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
+        int value = totalWaterDP(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
             System.out.print(value);
     }
+
+    /**
+     * Brute force solution:
+     * In brute force solution we do the same thing we read above
+     * 1. We go over each value using a loop
+     * 2. now inside inner loop we find out the left max height by going from that value towards the left most record left_max = Integer.max(input[j],left_max);
+     * 3. now we find right_max by going from the value towards right
+     * 4. once we have left_max and right_max for that specific value we calculate its accumulated water unit by formula min(left_max,right_max) - height(i)
+     * @param input
+     * @return
+     */
     public static int totalWater(int[] input){
         int output = 0;
         for(int i=0;i<input.length;i++){
@@ -42,6 +53,14 @@ public class TrappingRainWater_42 {
         }
         return output;
     }
+
+    /**
+     * Using dynamic programming solution we just do the same as we are doing in brute force solution. THe difference is that
+     * once we calculate the left_max and right_max for a value we save them in arrays.
+     * once we have these arrays we go over the main array and these 2 arrays and use formula min(left_max,right_max) - height(i) to calculate water units for each value.
+     * @param input
+     * @return
+     */
     public static int totalWaterDP(int[] input){
         int output = 0;
         int[] leftMax = new int[input.length];
@@ -49,6 +68,13 @@ public class TrappingRainWater_42 {
 
         for(int i=1;i<input.length;i++){
             leftMax[i] = Integer.max(input[i], leftMax[i-1]);
+        }
+        for(int j=input.length-2;j>0;j--){
+            rightMax[j] = Integer.max(input[j], rightMax[j+1]);
+        }
+        for(int k=0;k<input.length;k++){
+            if((Integer.min(leftMax[k],rightMax[k]) - input[k])>0)
+                output = output + Integer.min(leftMax[k],rightMax[k]) - input[k];
         }
         return output;
     }
